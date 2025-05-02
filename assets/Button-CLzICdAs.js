fadbb3cc776ab37d962730a21e84dc4064cbcfa1
fares sonar <faressonar678@gmail.com>
2025-05-02 12:57:38 +0300

    Updates

---
+++ assets/Button-CLzICdAs.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DesovQTb.js";const o=({text:t})=>e.jsx("button",{className:"bg-red-500 text-white py-4 px-8  rounded-[4px]",children:t});export{o as B};
---
+++ assets/formatCurrency-Be6qMWYI.js
@@ -0,0 +1,1 @@
+const e=new Intl.NumberFormat(void 0,{currency:"USD",style:"currency"}),n=r=>e.format(r);export{n as f};
