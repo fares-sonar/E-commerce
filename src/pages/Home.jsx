451fa7d1e5238bb3fa723e8ac3cecca76a1d1f36
fares sonar <faressonar678@gmail.com>
2025-05-03 11:36:55 +0300

    remove lazy from home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
-import { lazy } from "react";
 import FetchDataContext from "../Context/FetchDataContext";
-const lazyLoad = (path) => lazy(() => import(`../components/${path}`));
-const Hero = lazyLoad("2-Hero/Hero.jsx");
-const Products = lazyLoad("3-Products/Products.jsx");
-const Category = lazyLoad("4-Category/Category.jsx");
-const Selling = lazyLoad("5-Selling/Selling.jsx");
-const Music = lazyLoad("6-Music/Music.jsx");
-const Explore = lazyLoad("7-Explore/Explore.jsx");
-const Arrival = lazyLoad("8-Arrival/Arrival.jsx");
-const Services = lazyLoad("9-Services/Services.jsx");
+import {
+  Hero,
+  Products,
+  Category,
+  Selling,
+  Music,
+  Explore,
+  Arrival,
+  Services,
+} from "../components";
 
 function Home() {
   return (
